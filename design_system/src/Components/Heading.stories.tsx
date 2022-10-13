4cec5218bcdbf6cb0db7977be6935f034a01da39
LorrayneCardozo <lorraynecardozo15@gmail.com>
2022-10-13 15:06:57 -0300

    Heading component

---
+++ design_system/src/Components/Heading.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from './Heading';
+
+export default {
+    title: 'Components/Heading',
+    component: Heading,
+    args: {
+        children: 'Ola mundo!'
+    }
+} as Meta<HeadingProps>
+
+export const Default: StoryObj<HeadingProps> = {}
+
+export const Small: StoryObj<HeadingProps> = {
+    args:{
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<HeadingProps> = {
+    args:{
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+    args:{
+        asChild: true,
+        children: <h1>Component Customizado</h1>
+    }
+}
